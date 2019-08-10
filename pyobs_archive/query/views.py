1888eb0b87b7ebb7b287aa9baf68c0ef69d5e3f1
Tim-Oliver Husser <thusser@uni-goettingen.de>
2019-08-10 22:06:06 +0200

    initial commit

---
+++ pyobs_archive/query/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+from django.template import loader
+
+
+def index(request):
+    template = loader.get_template('query/index.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
