e378ff16010c8a2e366ae324ce41cf362fa27857
Tim-Oliver Husser <thusser@uni-goettingen.de>
2019-08-17 09:19:51 +0200

    using webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     entry: './src/main.js',
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'static')
     },
     resolve: {
