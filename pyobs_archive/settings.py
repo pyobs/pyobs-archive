654ef9b502dceea37b4627746b96ebc3e4113ad6
Tim-Oliver Husser <thusser@uni-goettingen.de>
2021-08-13 15:36:13 +0200

    added cors headers

--- pyobs_archive/settings.py
+++ pyobs_archive/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
     'pyobs_archive.api',
     'pyobs_archive.authentication',
     'pyobs_archive.frontend'
@@ -60,6 +61,7 @@ AUTHENTICATION_BACKENDS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -137,6 +139,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = '/static/'
+
+# allow access from other pages, e.g. portal
+CORS_ALLOW_ALL_ORIGINS = True
 
 # logging
 LOGGING = {
