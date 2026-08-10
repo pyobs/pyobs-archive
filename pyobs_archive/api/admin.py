afe60b08efa7968baca357e27d390c4325f9bd8a
Tim-Oliver Husser <thusser@uni-goettingen.de>
2026-08-10 17:34:35 +0200

    Add ModelAdmin customization for Frame
    admin.site.register(Frame) used the bare default admin, which does
    full-table queries as the list grows. Adds list_display, list_filter,
    and search_fields.

--- pyobs_archive/api/admin.py
+++ pyobs_archive/api/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Frame
 
-admin.site.register(Frame)
+
+@admin.register(Frame)
+class FrameAdmin(admin.ModelAdmin):
+    list_display = ('basename', 'SITEID', 'TELID', 'INSTRUME', 'IMAGETYP', 'RLEVEL', 'DATE_OBS')
+    list_filter = ('SITEID', 'TELID', 'INSTRUME', 'IMAGETYP', 'RLEVEL')
+    search_fields = ('basename', 'OBJECT', 'REQNUM', 'OBSNUM')
